71d5133bae8d7f96e224d0e445b6e58c5c5d6743
Tiago Sacramento <tiago.oliveira@igti.edu.br>
2023-02-11 08:37:00 -0300

    feat: add product service

---
+++ src/interfaces/product.ts
@@ -0,0 +1,12 @@
+export default interface Product {
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage?: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images?: string;
+}
---
+++ src/services/product-service.ts
@@ -0,0 +1,72 @@
+//import { CustomerDTO } from '../models/dto/customer.dto';
+import { AxiosResponse } from 'axios';
+import api from 'helpers/api';
+
+import Product from 'interfaces/product';
+
+interface Data {
+  limit: number;
+  skip: number;
+  total: number;
+}
+
+interface ResponseRequest<T> {
+  status: number;
+  data: T;
+}
+
+interface ProductData extends Data {
+  products: Product[];
+}
+
+export interface GetProductsResponse extends ResponseRequest<ProductData> {}
+
+//const newBusinessUrl = process.env.REACT_APP_NEW_BUSINESS_URL;
+
+const CreateProduct = async (params: Product) => {
+  return await api.post(`products/add`, params);
+};
+
+const GetProduct = async (productId: number) => {
+  return await api.get(`products/${productId}`);
+};
+
+const GetProducts = async () => {
+  return await api.get(`products/?limit=10`);
+};
+
+/*
+fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    
+  }),
+})
+  .then((res) => res.json())
+  .then(console.log);
+*/
+/*
+const UpdateProduct = async (
+  applicationId,
+  versionNumber,
+  customerId,
+  params: CustomerDTO
+) => {
+  return await api.put(
+    `${newBusinessUrl}applications/${applicationId}/versions/${versionNumber}/customers/${customerId}`,
+    params.customer
+  );
+};
+
+
+*/
+const productService = {
+  CreateProduct,
+  //UpdateProduct,
+  GetProduct,
+  GetProducts,
+};
+
+export default productService;
