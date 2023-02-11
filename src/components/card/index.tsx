52ee18ca8942c1fd677628639acd3b5a7470999d
Tiago Sacramento <tiago.oliveira@igti.edu.br>
2023-02-11 19:19:34 -0300

    refactor: add call to dialog

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,39 +6,50 @@ import {
   Buttons,
   Container,
   Content,
+  Footer,
   Image,
   Price,
   Prices,
   Title,
 } from './style';
+import { useDialogDispatch } from 'hooks/useDialog';
+import { SHOW_CONFIRM } from 'interfaces/dialog';
 
-interface CardProps {
+interface CardProps extends React.BaseHTMLAttributes<any> {
+  productId: number;
   thumbnail?: string;
 }
 
-export default function Card({ thumbnail }: CardProps) {
+export default function Card({ thumbnail, productId, ...props }: CardProps) {
+  const dialog = useDialogDispatch();
+
   return (
-    <Container onClick={() => console.log('Card clicked!')}>
+    <Container {...props}>
       <Image src={thumbnail} alt="" srcSet={thumbnail} />
       <Content>
         <Title>Product</Title>
-        <Prices>
-          <Price>100</Price>
-          <Price>40</Price>
-        </Prices>
+        <Footer>
+          <Prices>
+            <Price>100</Price>
+            <Price>40</Price>
+          </Prices>
+          <Buttons style={{ justifyContent: 'flex-end' }}>
+            <Button
+              color="#F80059"
+              onClick={async (e) => {
+                dialog({
+                  type: SHOW_CONFIRM,
+                  message: 'Deseja remover este produto?',
+                  onConfirm: () => console.log('REMOVIDO'),
+                });
+              }}
+            >
+              <FiTrash2 color="#FFF" />
+            </Button>
+            <Button>Editar</Button>
+          </Buttons>
+        </Footer>
       </Content>
-      <Buttons style={{ justifyContent: 'flex-end' }}>
-        <Button color="#F80059">
-          <FiTrash2 color="#FFF" />
-        </Button>
-        <Button
-          onClick={(e) => {
-            console.log('Button clicked!');
-          }}
-        >
-          Editar
-        </Button>
-      </Buttons>
     </Container>
   );
 }
