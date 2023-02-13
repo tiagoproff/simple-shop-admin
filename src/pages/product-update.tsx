270cdf937a52ff8ee37879793aeed6ed52cddecf
Tiago Sacramento <tiago.oliveira@igti.edu.br>
2023-02-13 07:36:27 -0300

    refactor: FormProductUpdate get only id

--- src/pages/product-update.tsx
+++ src/pages/product-update.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import FormProductUpdate from 'components/forms/form-product-update';
 
 export default function ProductUpdate() {
+  const [productId, setProductId] = useState<number>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const idNumber = Number(id);
+
+    if (Number.isInteger(idNumber)) {
+      setProductId(idNumber);
+    }
+  }, []);
+
   return (
     <div style={{ display: 'flex', flex: 1 }} id="product-details">
-      <FormProductUpdate />
+      {productId !== undefined ? (
+        <FormProductUpdate productId={productId} />
+      ) : null}
     </div>
   );
 }
